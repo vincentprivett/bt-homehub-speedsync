ad0154641bcbc4f6752fc488441d9f1555847c29
Vincent Privett <Vincent Privett>
2021-08-09 15:33:18 +0000

    added some comments ref regex to explain what iss happening

--- speedsync.py
+++ speedsync.py
@@ -10,16 +10,15 @@ os.chdir(sys.path[0]) # set working directory to save csv when run as cron job
 url = 'http://192.168.1.254/cgi/cgi_basicStatus.js'
 data = requests.get(url)
 
-# https://stackoverflow.com/questions/47515137/extracting-data-from-javascript-var-inside-script-with-python
+# Extract Line Status from BT HomeHub
 linestatus = re.search('var linestatus = (.+)[,;]{1}', data.text).group(1) # extract line status
-
+# https://stackoverflow.com/questions/47515137/extracting-data-from-javascript-var-inside-script-with-python
+lsRegex = re.compile(r"(\w+):'(.+?)'")
 '''
 regex to extract the values from linestatus
 group 1 (key) matches any word character one or more times
 group 2 (value) mathces any char one or more times
 '''
-
-lsRegex = re.compile(r"(\w+):'(.+?)'")
 lsItems = lsRegex.findall(linestatus)
 lsKey = []
 lsValue = []
@@ -27,6 +26,8 @@ for x in range(len(lsItems)):
     lsKey.append(lsItems[x][0]) # buld list of line status keys from .js
     lsValue.append(lsItems[x][1]) # buld list of line status values from .js
 
+
+# Build CSV file
 time = time.strftime('%d-%m-%Y %H:%M:%S') # assigns time to use in csv when adding values
 file_exists = os.path.isfile('syncresults.csv')
 with open('syncresults.csv', "a") as csvfile:
